74bfd2ba674319fefe197b60a0fea7ecf3f56758
davi-OD <davidodie51@gmail.com>
2021-03-22 06:26:51 +0300

    Updated work on stayclean initative, pug fles and public

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 //Dependencies
 const express = require('express');
 
-//Imstantiations
+//Instantiations
 const app = express();
 
 //Configurations
 app.set('view engine', 'pug');
 app.set('views', './views');
+
 //Middleware
 app.use('/static', express.static('public')); //for static pages
-app.use(express.static('public'));
-app.use(express.static('files'));
-//Routes
-app.get('/', (req, res) => res.send('Hello World!'));
 
-//app.post('login', (req,res) => res.send('login received'));
+
+//Routes
+//app.get('/', (req, res) => 
+
+app.get('/', (req,res) => res.send('Welcome'));
+
+app.get('/newEmployee', (req, res) => 
+    res.render('newEmployee'));
+
+
 
 
 //Bootstrapping Server
